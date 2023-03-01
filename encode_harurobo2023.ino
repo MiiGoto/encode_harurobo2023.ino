a13c04718d0db3ea46182297cd5283ca8e90bf16
mii <109723126+MiiGoto@users.noreply.github.com>
2023-03-02 07:58:51 +0900

    Create encode_harurobo2023.ino

---
+++ encode_harurobo2023.ino
@@ -0,0 +1,27 @@
+#include <FlexCAN.h>
+
+FlexCAN CANTransmitter(1000000);
+CAN_message_t msg;
+
+int servo = 1;
+int syl1 = 0;
+int syl2 = 1;
+int motor = 5;
+  
+void setup(void)
+{
+  CANTransmitter.begin();
+  Serial.begin(115200);
+  delay(1000);
+
+  msg.len = 8;
+  msg.id = 0x200;
+  for ( int idx = 0; idx < msg.len; ++idx ) {
+    msg.buf[idx] = 0;
+  }
+}
+
+void loop(void){
+  buf[0]= servo+syl1*2+syl2*4+(motor>0)*8+abs(motor)*16;
+}
+
