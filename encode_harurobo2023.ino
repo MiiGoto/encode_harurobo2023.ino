fca5ef20a4734d6bf990d362524a8f120d4163ae
mii <109723126+MiiGoto@users.noreply.github.com>
2023-03-02 09:53:00 +0900

    Update encode_harurobo2023.ino

--- encode_harurobo2023.ino
+++ encode_harurobo2023.ino
@@ -8,20 +8,10 @@ int syl1 = 0;
 int syl2 = 1;
 int motor = 5;
   
-void setup(void)
-{
-  CANTransmitter.begin();
-  Serial.begin(115200);
-  delay(1000);
-
-  msg.len = 8;
-  msg.id = 0x200;
-  for ( int idx = 0; idx < msg.len; ++idx ) {
-    msg.buf[idx] = 0;
-  }
+void setup(){
 }
 
-void loop(void){
+void loop(){
   msg.buf[0]= servo+syl1*2+syl2*4+(motor>0)*8+abs(motor)*16;
 }
 
